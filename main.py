7b06959ee4dd2b98b884303cb20c3f2aefe75f37
Rajnandan Rajbanshi <83878581+rajnandan-web@users.noreply.github.com>
2024-02-05 10:05:47 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+class Library:
+    def __init__(self,ListOfBooks):
+        self.Books=ListOfBooks
+
+    def DisplayAvailableBooks(self,BookName):
+        if self.Books==BookName:
+            print("\t * " +BookName)
+
+
+
+
+
+class Vendors:
+    pass
+
+
+
+if __name__ ==" __main__":
+    CentralLibrary=self.ListOfBooks(["Data Science","Data Structure","Algorithm","CTRLS"])
+    print("The available books are: ")
+    DisplayAvailableBooks.ListOfBooks()
+
